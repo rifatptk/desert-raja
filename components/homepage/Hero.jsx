376df8ff17493dda672605dd27b2f9881cc293c5
rifatptk <rifatptk@gmail.com>
2022-11-17 14:45:09 +0600

    console errors fixed

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -4,13 +4,13 @@ const Hero = () => {
       <video
         width="100%"
         height="100%"
-        class="bg-video"
+        className="bg-video"
         src="https://embed-ssl.wistia.com/deliveries/24cc844626ee37fd48be6611d0a844b35fe3a2f3/file.mp4"
         type="video/mp4"
         preload="auto"
-        autoplay="true"
+        autoPlay={true}
         loop="loop"
-        muted="muted"
+        muted={true}
         poster=""
       >
         <source
