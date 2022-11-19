4d60b51da592463eea6ea6f63f187dd14ef150de
rifatptk <rifatptk@gmail.com>
2022-11-19 10:04:24 +0600

    insta updated

--- components/homepage/instagram/Instagram.jsx
+++ components/homepage/instagram/Instagram.jsx
@@ -1,3 +1,26 @@
+const instaFrames = [
+  {
+    id: 1,
+    src: 'https://www.instagram.com/tv/CVd0SkHP_dM/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A2%2C%22os%22%3A4591%2C%22ls%22%3A3346.300000000745%2C%22le%22%3A3701%7D',
+  },
+  {
+    id: 2,
+    src: 'https://www.instagram.com/p/CUhyhy3lVOb/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A3%2C%22os%22%3A4795.600000023842%2C%22ls%22%3A1902.4000000059605%2C%22le%22%3A2348.800000011921%7D',
+  },
+  {
+    id: 3,
+    src: 'https://www.instagram.com/p/CUHzvg8M31t/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A4%2C%22os%22%3A5558.9000000059605%2C%22ls%22%3A1902.4000000059605%2C%22le%22%3A2348.800000011921%7D',
+  },
+  {
+    id: 4,
+    src: 'https://www.instagram.com/reel/CV6KGPHh-6I/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A0%2C%22os%22%3A2964%2C%22ls%22%3A1902.4000000059605%2C%22le%22%3A2348.800000011921%7D',
+  },
+  {
+    id: 5,
+    src: 'https://www.instagram.com/reel/CVz1mawNyW4/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A1%2C%22os%22%3A2967%2C%22ls%22%3A1902.4000000059605%2C%22le%22%3A2348.800000011921%7D',
+  },
+];
+
 const Instagram = () => {
   return (
     <section id="instagram" className="my-20">
@@ -5,34 +28,16 @@ const Instagram = () => {
         Video Testimonials from our customers
       </h2>
       <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
-        <iframe
-          className="mx-auto"
-          width="320"
-          height="440"
-          src="https://www.instagram.com/tv/CVd0SkHP_dM/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A2%2C%22os%22%3A4591%2C%22ls%22%3A3346.300000000745%2C%22le%22%3A3701%7D"
-          frameborder="0"
-        />
-        <iframe
-          className="mx-auto"
-          width="320"
-          height="440"
-          src="https://www.instagram.com/tv/CVd0SkHP_dM/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A2%2C%22os%22%3A4591%2C%22ls%22%3A3346.300000000745%2C%22le%22%3A3701%7D"
-          frameborder="0"
-        />
-        <iframe
-          className="mx-auto"
-          width="320"
-          height="440"
-          src="https://www.instagram.com/tv/CVd0SkHP_dM/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A2%2C%22os%22%3A4591%2C%22ls%22%3A3346.300000000745%2C%22le%22%3A3701%7D"
-          frameborder="0"
-        />
-        <iframe
-          className="mx-auto"
-          width="320"
-          height="440"
-          src="https://www.instagram.com/tv/CVd0SkHP_dM/embed/?cr=1&v=14&wp=326&rd=https%3A%2F%2Fdesertraja.com&rp=%2F#%7B%22ci%22%3A2%2C%22os%22%3A4591%2C%22ls%22%3A3346.300000000745%2C%22le%22%3A3701%7D"
-          frameborder="0"
-        />
+        {instaFrames.map((el) => (
+          <iframe
+            key={el.id}
+            className="mx-auto"
+            width="320"
+            height="440"
+            src={el.src}
+            frameBorder="0"
+          />
+        ))}
       </div>
       <div className="h-20"></div>
       <a
